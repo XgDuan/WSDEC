f3e437cbaec3bd47fc2730d265843f3534e3e3a9
XgDuan <dxg14@mails.tsinghua.edu.cn>
2018-11-06 12:13:05 +0800

    Modified: download.sh

--- data/download.sh
+++ data/download.sh
@@ -6,14 +6,7 @@ wget -c http://ec2-52-11-11-89.us-west-2.compute.amazonaws.com/data/challenge16/
 wget -c http://ec2-52-11-11-89.us-west-2.compute.amazonaws.com/data/challenge16/features/c3d/activitynet_v1-3.part-03 1>/dev/null 2>&1 &
 wget -c http://ec2-52-11-11-89.us-west-2.compute.amazonaws.com/data/challenge16/features/c3d/activitynet_v1-3.part-04 1>/dev/null 2>&1 &
 wget -c http://ec2-52-11-11-89.us-west-2.compute.amazonaws.com/data/challenge16/features/c3d/activitynet_v1-3.part-05 1>/dev/null 2>&1 & 
-# wget http://ec2-52-11-11-89.us-west-2.compute.amazonaws.com/data/challenge16/features/c3d/activitynet_v1-3.part-05 & 
 
-# wait %0
-# wait %1
-# wait %2
-# wait %3
-# wait %4
-# wait %5
 wait 
 # merge them into a single hdf5 file
 echo "\033[32;40mmerge datasets into a single hdf5 file(minutes)\033[0m"
